728a092586518366a9be7b867831a586c55de832
woogi <azicplus@naver.com>
2025-11-11 09:14:58 +0900

    Initial commit

---
+++ ToneMeter/ToneMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneMeterApp.swift
+//  ToneMeter
+//
+//  Created by 송형욱 on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ToneMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToneMeterTests/ToneMeterTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToneMeterTests.swift
+//  ToneMeterTests
+//
+//  Created by 송형욱 on 11/11/25.
+//
+
+import Testing
+@testable import ToneMeter
+
+struct ToneMeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
